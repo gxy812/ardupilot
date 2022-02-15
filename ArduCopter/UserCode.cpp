510bb5c650122270b1c5557ebff318f0617dddee
gxy812 <56009756+gxy812@users.noreply.github.com>
2022-02-15 13:26:46 +0800

    Final

--- ArduCopter/UserCode.cpp
+++ ArduCopter/UserCode.cpp
@@ -1,6 +1,7 @@
 #include "Copter.h"
 #include "hal_pal_lld.h"
 #include "hwdef.h"
+#include <sys/_stdint.h>
 
 #ifdef USERHOOK_INIT
 void Copter::userhook_init()
@@ -8,55 +9,51 @@ void Copter::userhook_init()
     // put your initialisation code here
     // this will be called once at start-up
 
-    // turn on GPIO pin for use as 3v3
-    hal.gpio->pinMode(54, HAL_GPIO_OUTPUT);
-    hal.gpio->write(54, TRUE);
-
-    // add time for devices to turn on
-    hal.scheduler->delay(100);
-
     // mux init
     const uint8_t I2C_SLAVE_DEVICE_ADDRESS = 0x8A;
     auto i2c_bus = hal.i2c_mgr->get_device(3, 0x70);
 
     uint8_t default_address = 50;
-    uint8_t temp = 0xFF;
-    uint8_t final_address = 0;
+    uint8_t temp = 0x00;
     uint8_t found = 0;
     i2c_bus->get_semaphore()->take_blocking();
     i2c_bus->set_retries(10);
-    // test by enabling all i2c ports on mux
-    // allows debugging using lua scripts (i2c_scan) if anything fails
+    // test by disabling all i2c ports on mux
     if (i2c_bus->transfer(&temp, 1, nullptr, 0)) {
         // assign addresses
-        for (uint8_t i = 0; i < 8; i++) {
+        for (uint8_t i = 0; i <= 7; i++) {
             uint8_t local_address = 1 << i;
-            i2c_bus->set_address(0x70);
+            // release and take semaphores on address change
             i2c_bus->transfer(&local_address, 1, nullptr, 0);
             hal.scheduler->delay(50);
+            i2c_bus->get_semaphore()->give();
             i2c_bus->set_address(0x29);
+            i2c_bus->get_semaphore()->take_blocking();
             if (i2c_bus->write_register(I2C_SLAVE_DEVICE_ADDRESS, default_address)) {
                 hal.console->printf("Device %d address is now %d", i, default_address);
-                final_address |= local_address;
+                mux_address_used_flag |= 1 << (default_address - 50);
                 default_address++;
                 found++;
             }
+            i2c_bus->get_semaphore()->give();
+            i2c_bus->set_address(0x70);
+            i2c_bus->get_semaphore()->take_blocking();
         }
         temp = 0xFF;
-        i2c_bus->transfer(&final_address, 1, nullptr, 0); // connect all channels
+        i2c_bus->transfer(&temp, 1, nullptr, 0); // connect all channels
         hal.scheduler->delay(50);
         hal.console->printf("Found %d devices on mux", found);
+        i2c_bus->get_semaphore()->give();
         i2c_bus->set_address(0x29);
+        i2c_bus->get_semaphore()->take_blocking();
         if (i2c_bus->read_registers(I2C_SLAVE_DEVICE_ADDRESS, &temp, 1)) {
             hal.console->printf("\nProblem assigning addresses!\n");
         }
+        i2c_bus->get_semaphore()->give();
     }
     else {
         hal.console->println("No devices found!");
     }
-    i2c_bus->set_address(0x70);
-    i2c_bus->get_semaphore()->give();
-    hal.scheduler->delay(500);
 }
 #endif
 
@@ -91,8 +88,22 @@ void Copter::userhook_SlowLoop()
 #ifdef USERHOOK_SUPERSLOWLOOP
 void Copter::userhook_SuperSlowLoop()
 {
+    const uint8_t I2C_SLAVE_DEVICE_ADDRESS = 0x8A;
     // put your 1Hz code here
     //hal.console->printf("\nFound %d devices on mux\n", Mux_found);
+    auto i2c_bus = hal.i2c_mgr->get_device(3, 0x29);
+    uint8_t temp;
+    i2c_bus->get_semaphore()->take_blocking();
+    // "hot replug" capability -- although not advisable when more than one device is being replugged
+    if (i2c_bus->read(&temp, 1)) {
+        uint8_t free_addresses = ~mux_address_used_flag;
+        for (uint8_t i = 0; i < 8; i++) {
+            if ((1 << i) & free_addresses) {
+                i2c_bus->write_register(I2C_SLAVE_DEVICE_ADDRESS, 50 + i);
+            }
+        }
+    }
+    i2c_bus->get_semaphore()->give();
 }
 #endif
 
--- ArduCopter/UserVariables.h
+++ ArduCopter/UserVariables.h
@@ -2,4 +2,6 @@
 
 #ifdef USERHOOK_VARIABLES
 
+uint8_t mux_address_used_flag = 0;
+
 #endif // USERHOOK_VARIABLES
